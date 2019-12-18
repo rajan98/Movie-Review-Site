0dfe9e69764342470a2b201d2a85caa6cda3eec5
Rajan Kumar Shah <rajan.kum.sha@gmail.com>
2019-12-18 11:14:04 +0530

    Add files via upload
    Login Validation Added

--- src/app/login/login.component.ts
+++ src/app/login/login.component.ts
@@ -7,9 +7,54 @@ import { Component, OnInit } from '@angular/core';
 })
 export class LoginComponent implements OnInit {
 
+  loginChecker: number;
+  loginButtonClick: boolean = false;
+  loginSuccess: boolean = false;
+  username: string;
+  password: string;
+
+  loginDetail = [
+    {
+      username : "admin",
+      password :  "admin"
+    },
+    {
+      username : "user",
+      password : "user"
+    },
+    {
+      username : "tom",
+      password : "mot",
+    },
+    {
+      username : "Raj",
+      password : "jaR"
+    }
+  ]
+
   constructor() { }
 
   ngOnInit() {
+  }
+
+  loginFunction(){
+    
+    for(let i=0;i<this.loginDetail.length; i++){
+      if(this.username == this.loginDetail[i].username && this.password == this.loginDetail[i].password){
+        this.loginSuccess = true;
+        if(this.username == 'admin'){
+          console.log("Admin successfully logged in");
+        }
+        else{
+          console.log( this.username + " Successfully logged in");
+        }
+      }
+    }
+    if(!this.loginSuccess){
+      this.loginSuccess = false;
+      console.log("Unsuccessful Login");
+      this.loginButtonClick = true;
+    }
   }
 
 }
